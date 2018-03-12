bf5cca7dcf4ea755b0ea831bc49437b8710d1be8
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-03-13 02:28:58 +0300

    Add files via upload

---
+++ RotateByObject.cs
@@ -0,0 +1,12 @@
+﻿//ака 47
+using UnityEngine;
+
+public class RotateByObject : MonoBehaviour {
+
+	[SerializeField] protected Vector3 vc3 = new Vector3(0,0,0);
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (vc3.x,vc3.y,vc3.z);
+	}
+}
