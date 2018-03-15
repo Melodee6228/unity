acedc148162abc6ae17d995c4495bb12eb502a5e
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-03-15 06:52:30 +0300

    Add files via upload

---
+++ PointRotateByPlayer.cs
@@ -0,0 +1,25 @@
+﻿//ака 47
+using UnityEngine;
+
+public class PointRotateByPlayer : MonoBehaviour {
+
+	[SerializeField] protected Vector3[] Distance = null;
+	[SerializeField] protected Transform Point = null;
+	[SerializeField] protected Transform[] PointObject = null;
+	[SerializeField] protected Vector3 Vc3 = new Vector3(0,1,0);
+
+	void Start()
+	{
+		for(int a = 0 ; a < PointObject.Length ; a++)
+		{
+			PointObject [a].transform.localPosition = new Vector3(Distance[a].x,Distance[a].y,Distance[a].z);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Point.transform.localPosition = this.transform.localPosition;
+		Point.Rotate (Vc3.x, Vc3.y , Vc3.z);
+	}
+}
